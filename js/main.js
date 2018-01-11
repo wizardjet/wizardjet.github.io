38000badbb8c8952a1a2b4d4a21abd8de91a4e53
Justin Yang <justinyang28@googlemail.com>
2018-01-11 11:42:13 +0000

    got rid of PHP stuff and composer stuff and added JS mailer instead

--- js/main.js
+++ js/main.js
@@ -224,42 +224,26 @@
 
 			var sLoader = $('#submit-loader')
 
-			$.ajax({      	
-
-				type: "POST",
-				url: "inc/sendEmail.php",
-				data: $(form).serialize(),
-				beforeSend: function() { 
-  
-					sLoader.fadeIn(); 
-  
-				},
-				success: function(msg) {
-  
-				  // Message was sent
-				  if (msg == 'OK') {
-					  sLoader.fadeOut(); 
-					 $('#message-warning').hide();
-					 $('#contactForm').fadeOut();
-					 $('#message-success').fadeIn();   
-				  }
-				  // There was an error
-				  else {
-					  sLoader.fadeOut(); 
-					 $('#message-warning').html(msg);
-					  $('#message-warning').fadeIn();
-				  }
-  
-				},
-				error: function() {
-  
-					sLoader.fadeOut(); 
-					$('#message-warning').html("Something went wrong. Please try again.");
-				   $('#message-warning').fadeIn();
-  
-				}
-  
-			});
+		//JS Email
+		// parameters: service_id, template_id, template_parameters
+		var name = $('contactName').val() + " " + $('contactEmail').val();
+		var notes = $('contactMessage').val();
+		emailjs.send("gmail_account","websiteemail",{
+			"name": $('#contactName').val(), 
+			"email": $('#contactEmail').val(),
+			"subject": $('#contactSubject').val(),
+			"content": $('#contactMessage').val()
+		}
+		)
+		.then(function(response) {
+			  sLoader.fadeOut(); 
+			 $('#message-warning').hide();
+			 $('#contactForm').fadeOut();
+			 $('#message-success').fadeIn(); 
+		}, function(err) {
+			$('#message-warning').html("Something went wrong. Please try again.");
+			$('#message-warning').fadeIn();
+		});
   		}
 
 	});
